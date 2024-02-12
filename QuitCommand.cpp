0e3225658e8407bd625e91212746e58590a94f09
MahamidOmar <omarm@campus.technion.ac.il>
2024-02-13 00:17:03 +0200

    v 1.2 Finished Quit Command Still working on external

--- QuitCommand.cpp
+++ QuitCommand.cpp
@@ -1,3 +1,15 @@
-//
-// Created by ebrah on 12/02/2024.
-//
+#include "QuitCommand.h"
+#include "Helper.h"
+
+void QuitCommand::execute() {
+    string current_command_trimmed = _trim(this->command_line);
+    ////    This checks that the quit command recieved at least one parameter
+    if(current_command_trimmed.find_first_of(" \n") != string::npos){
+        string command_parameters = _trim(current_command_trimmed.substr(current_command_trimmed.find_first_of(" \n")));
+        if(command_parameters.substr(0, command_parameters.find_first_of(" \n")) == "kill"){
+            cout << "smash: sending SIGKILL signal to " << this->allJobs.size() << " jobs:" << endl;
+            this->all_jobs->killAllJobs();
+        }
+        exit(0);
+    }
+}
