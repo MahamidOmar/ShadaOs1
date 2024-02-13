3e7ae5fc9e283f90a8053729d2e3baeaadaae789
Saed ebrahim <ebrahim-saed@campus.technion.ac.il>
2024-02-13 12:04:27 +0200

    v 1.4 day 2

--- ChangeDirCommand.cpp
+++ ChangeDirCommand.cpp
@@ -1,3 +1,9 @@
 //
 // Created by ebrah on 12/02/2024.
 //
+#include "ChangeDirCommand.h"
+
+void ChangeDirCommand::execute() {
+
+}
+
--- RedirectionCommand.cpp
+++ RedirectionCommand.cpp
@@ -1,3 +1,10 @@
 //
 // Created by ebrah on 12/02/2024.
 //
+#include "RedirectionCommand.h"
+#include "Helper.h"
+
+void RedirectionCommand::execute() {
+
+}
+
