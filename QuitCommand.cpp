802ebb28e7e086e07d98076884613448ab525aea
Saed ebrahim <ebrahim-saed@campus.technion.ac.il>
2024-02-18 14:18:27 +0200

    v 4.2 change quit

--- QuitCommand.cpp
+++ QuitCommand.cpp
@@ -1,13 +1,11 @@
 #include "QuitCommand.h"
 
 void QuitCommand::execute() {
-    string current_command_trimmed = _trim(this->command_line);
+    string currentCommandTrimmed = _trim(this->command_line);
 
-    ////    This checks that the quit command recieved at least one parameter
-    if(current_command_trimmed.find_first_of(" \n") != string::npos){
-        string command_parameters = _trim(current_command_trimmed.substr(current_command_trimmed.find_first_of(" \n")));
-        if(command_parameters.substr(0, command_parameters.find_first_of(" \n")) == "kill"){
-//            cout << "smash: sending SIGKILL signal to " << this->all_jobs.size() << " jobs:" << endl;
+    if(currentCommandTrimmed.find(" ") != string::npos){
+        string commandParameters = _trim(currentCommandTrimmed.substr(currentCommandTrimmed.find(" ") + 1));
+        if(commandParameters == "kill"){
             this->all_jobs->killAllJobs();
         }
     }
