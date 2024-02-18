ce2a09ac3bbc2d8d2140a7a02c25bb0d15ff6e29
Saed ebrahim <ebrahim-saed@campus.technion.ac.il>
2024-02-18 12:13:53 +0200

    v 3.8 change chmod +external

--- ExternalCommand.cpp
+++ ExternalCommand.cpp
@@ -1,52 +1,99 @@
 //
 // Created by ebrah on 12/02/2024.
 //
+//#include "ExternalCommand.h"
+//#include "SmallShell.h"
+//
+//
+//void ExternalCommand::execute() {
+//    SmallShell &smash = SmallShell::getInstance();
+//    char* command = (char*)malloc(strlen(this->command_line.c_str())+1);
+//    strcpy(command, this->command_line.c_str());
+//    bool simple_command = true;
+//    if(this->command_line.find('?') != string::npos || this->command_line.find('*') != string::npos){
+//        simple_command = false;
+//    }
+//    char *parsed[COMMAND_MAX_ARGS + 1];
+//    _parseCommandLine(this->command_line.c_str(), parsed);
+//    if (_isBackgroundComamnd(command)) {
+//        _removeBackgroundSign(command);
+//    }
+//    _parseCommandLine(command, parsed);
+//    int pid;
+//    DO_SYS(pid = fork(), fork);
+//    if (pid == 0) {
+//        DO_SYS(setpgrp() , setpgrp);
+//        if (simple_command){
+//            if(execvp(parsed[0],parsed) == -1)
+//            {
+//                perror("smash error: execvp failed");
+//                exit(1);
+//            }
+//            //DO_SYS(execvp(parsed[0],parsed) , execvp );
+//        }else{
+//            char *argv[] = {"/bin/bash", "-c", command, nullptr};
+//            DO_SYS(execv("/bin/bash", argv), execv);
+//        }
+//        exit(0);
+//    }
+//    if( _isBackgroundComamnd(this->command_line.c_str()) ){
+//        smash.getJobList()->removeFinishedJobs();
+//        smash.getJobList()->addJob(this,pid,BACKGROUND);
+//    }
+//    else{
+//        smash.setLine(this->command_line);
+//        smash.setCurrPid(pid);
+//        DO_SYS(waitpid(pid, NULL, WUNTRACED) , waitpid);
+//        smash.setLine("") ;
+//        smash.setCurrPid(-1);
+//        smash.setCurrId(-1);
+//    }
+//}
+
 #include "ExternalCommand.h"
 #include "SmallShell.h"
 
-
 void ExternalCommand::execute() {
     SmallShell &smash = SmallShell::getInstance();
-    char* command = (char*)malloc(strlen(this->command_line.c_str())+1);
-    strcpy(command, this->command_line.c_str());
-    bool simple_command = true;
-    if(this->command_line.find('?') != string::npos || this->command_line.find('*') != string::npos){
-        simple_command = false;
-    }
+    std::string command = this->command_line;
+    bool isSimpleCommand = command.find('?') == std::string::npos && command.find('*') == std::string::npos;
+
     char *parsed[COMMAND_MAX_ARGS + 1];
-    _parseCommandLine(this->command_line.c_str(), parsed);
-    if (_isBackgroundComamnd(command)) {
-        _removeBackgroundSign(command);
+    _parseCommandLine(command.c_str(), parsed);
+
+    if (_isBackgroundComamnd(command.c_str())) {
+        _removeBackgroundSign(const_cast<char*>(command.c_str()));
     }
-    _parseCommandLine(command, parsed);
+
+    _parseCommandLine(command.c_str(), parsed);
+
     int pid;
     DO_SYS(pid = fork(), fork);
+
     if (pid == 0) {
-        DO_SYS(setpgrp() , setpgrp);
-        if (simple_command){
+        DO_SYS(setpgrp(), setpgrp);
+        if (isSimpleCommand){
             if(execvp(parsed[0],parsed) == -1)
             {
                 perror("smash error: execvp failed");
                 exit(1);
             }
-            //DO_SYS(execvp(parsed[0],parsed) , execvp );
-        }else{
-            char *argv[] = {"/bin/bash", "-c", command, nullptr};
+        } else {
+            char *argv[] = {"/bin/bash", "-c", const_cast<char*>(command.c_str()), nullptr};
             DO_SYS(execv("/bin/bash", argv), execv);
         }
         exit(0);
     }
-    if( _isBackgroundComamnd(this->command_line.c_str()) ){
+
+    if(_isBackgroundComamnd(command.c_str())){
         smash.getJobList()->removeFinishedJobs();
-        smash.getJobList()->addJob(this,pid,BACKGROUND);
-    }
-    else{
-        smash.setLine(this->command_line);
+        smash.getJobList()->addJob(this, pid, BACKGROUND);
+    } else {
+        smash.setLine(command);
         smash.setCurrPid(pid);
-        DO_SYS(waitpid(pid, NULL, WUNTRACED) , waitpid);
-        smash.setLine("") ;
+        DO_SYS(waitpid(pid, NULL, WUNTRACED), waitpid);
+        smash.setLine("");
         smash.setCurrPid(-1);
         smash.setCurrId(-1);
     }
 }
-
